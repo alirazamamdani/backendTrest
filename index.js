aa21e1b3581002a0c6e4ba1b61238e834b27aaa6
Ali Raza Mamdani <Ali.mamdani@hotmail.com>
2024-04-30 15:26:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 const express = require("express");
 const app = express();
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 const bp = require("body-parser");
 const PORT = process.env.PORT || 8000;
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI;
 app.use(cors());
 app.use(bp.urlencoded({ extended: false }));
 app.use(bp.json());
 
-// mongoose
-//   .connect(MONGODB_URI)
-//   .then(() => {
-//     console.log("Connected to MongoDB");
-//   })
-//   .catch((error) => {
-//     console.error("MongoDB connection error:", error);
-//   });
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
 
- app.get("/", (req, res) => {
-    res.send("Hello World")
- }) 
- 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is Running ${PORT}`);
